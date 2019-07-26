49521f68d72283d1468b3d4e48d32e1c9f8bd48a
Kate Morris <kmmorris@deloitte.co.uk>
2019-07-26 11:24:22 +0100

    Added delete

---
+++ JS The Best Bits/object_delete.js
@@ -0,0 +1,31 @@
+// Delete will not remove  
+
+
+if (typeof Object.create != 'function') {
+    Object.create = function (objectToBeCreated) {
+        var FunctionToCreate = function () {};
+        FunctionToCreate.prototype = objectToBeCreated;
+        return new FunctionToCreate();
+    };
+};
+
+var objectThing = {
+    name: 'Kate',
+    delegatedThing: 'delegatedThing',
+}
+
+var anotherThing = Object.create(objectThing);
+anotherThing.name = 'Stewart';
+console.log(objectThing.name);
+console.log(anotherThing.name);
+
+delete anotherThing.name;
+
+console.log(objectThing.name);
+console.log(anotherThing.name);
+
+anotherThing.name = 'Stewart';
+delete objectThing.name;
+
+console.log(objectThing.name);
+console.log(anotherThing.name);
