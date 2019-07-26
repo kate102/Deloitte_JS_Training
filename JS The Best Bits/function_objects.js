52b425159f4093ccd362c018c57b20cb73b65457
Kate Morris <kmmorris@deloitte.co.uk>
2019-07-26 16:55:54 +0100

    Started functions - chapter 4

---
+++ JS The Best Bits/function_objects.js
@@ -0,0 +1,23 @@
+// Functions are the FUNDAMENTAL MODULAR UNITS of JavaScript
+// A function encloses a set of Statements
+
+// Used for Code reuse
+// information hiding
+// and composition
+
+// Function objects are linked to Function.prototype 
+// (which in itself is linked to Object.prototype)
+
+// Functions also have 2 additional properties
+// 1. The functions context
+// 2. The code that implements the behaviour
+
+// Functions are objects and can therefore, 
+// be stored in variable's objects and arrays
+// They can be passed as arguments and returned from other functions
+
+
+// A method is a function that is a property of an object and not a stand-alone function in itself.
+// The think that is special about finctions is that they can be invoked
+// To activate. One usually speaks of invoking a function or routine in a program. 
+// In this sense, the term invoke is synonymous with call.
